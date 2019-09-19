3c1ba7ddbba389f7d4c324c3258695608f5d1972
Misa Jokisalo <misa.jokisalo@joinex.com>
2019-09-19 14:53:33 +0300

    Updates

---
+++ precache-manifest.ec697be4cc21ba2230203859a7a7862b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2541f01a0426de0499e7a7fe6a5b92c9",
+    "url": "/misacorp.io/index.html"
+  },
+  {
+    "revision": "d832279d20caa335fe8d",
+    "url": "/misacorp.io/static/js/2.ca71ead0.chunk.js"
+  },
+  {
+    "revision": "9e403d66926498fccd5d",
+    "url": "/misacorp.io/static/js/main.1cacc612.chunk.js"
+  },
+  {
+    "revision": "08e5aeb1830728ceafa1",
+    "url": "/misacorp.io/static/js/runtime~main.e346ff78.js"
+  }
+]);
