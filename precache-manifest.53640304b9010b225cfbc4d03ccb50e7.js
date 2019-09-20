31ed4148e3deadcef44741f5725bb3605a957433
Misa Jokisalo <misa.jokisalo@joinex.com>
2019-09-20 09:29:25 +0300

    Updates

---
+++ precache-manifest.53640304b9010b225cfbc4d03ccb50e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d69b1155c3748c849f97f4537f85d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "425a4cbbd88f5a7605e7",
+    "url": "/static/js/2.046b92ec.chunk.js"
+  },
+  {
+    "revision": "1da838758c78d00bb169",
+    "url": "/static/js/main.578bd6b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/misacorp.io/precache-manifest.ec697be4cc21ba2230203859a7a7862b.js"
+  "/precache-manifest.53640304b9010b225cfbc4d03ccb50e7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/misacorp.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
